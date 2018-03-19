9b29c84f09feaf2e38eae362d9b9f04e22749205
Roberto Desktop <diprob88@gmail.com>
2018-03-19 15:23:15 +0100

    pagination

--- QuirkyBookRental/QuirkyBookRental/Controllers/BookRentController.cs
+++ QuirkyBookRental/QuirkyBookRental/Controllers/BookRentController.cs
@@ -1,4 +1,5 @@
-﻿using QuirkyBookRental.Models;
+﻿using Microsoft.AspNet.Identity;
+using QuirkyBookRental.Models;
 using QuirkyBookRental.Utility;
 using QuirkyBookRental.ViewModel;
 using System;
@@ -6,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 
 namespace QuirkyBookRental.Controllers
 {
@@ -20,9 +22,61 @@ namespace QuirkyBookRental.Controllers
         }
 
         // GET: BookRent
-        public ActionResult Index()
+        public ActionResult Index(int? pageNumber, string option = null, string search = null)
         {
-            return View();
+            string userid = User.Identity.GetUserId();
+            var model = from br in db.BookRental
+                        join b in db.Books on br.BookId equals b.Id
+                        join u in db.Users on br.UserId equals u.Id
+                        select new BookRentalViewModel
+                        {
+                            BookId = b.Id,
+                            RentalPrice = br.RentalPrice,
+                            Price = b.Price,
+                            Pages = b.Pages,
+                            FirstName = u.FirstName,
+                            LastName = u.LastName,
+                            BirthDate = u.BirthDate,
+                            ScheduledEndDate = br.ScheduledEndDate,
+                            Author = b.Author,
+                            Avaibility = b.Avaibility,
+                            DateAdded = b.DateAdded,
+                            Description = b.Description,
+                            Email = u.Email,
+                            GenreId = b.GenreId,
+                            Genre = db.Genres.Where(g => g.Id.Equals(b.GenreId)).FirstOrDefault(),
+                            ISBN = b.ISBN,
+                            ImageUrl = b.ImageUrl,
+                            ProductDimensions = b.ProductDimensions,
+                            PubblicationDate = b.PubblicationDate,
+                            Publisher = b.Publisher,
+                            RentalDuration = br.RentalDuration,
+                            Status = br.Status.ToString(),
+                            Title = b.Title,
+                            UserId = u.Id,
+                            Id = br.Id,
+                            StartDate = br.StartDate
+                        };
+
+            if (option == "email" && search.Length > 0)
+            {
+                model = model.Where(u => u.Email.Contains(search));
+            }
+            if (option == "name" && search.Length > 0)
+            {
+                model = model.Where(u => u.FirstName.Contains(search) || u.LastName.Contains(search));
+            }
+            if (option == "status" && search.Length > 0)
+            {
+                model = model.Where(u => u.Status.Contains(search));
+            }
+
+            if (!User.IsInRole(SD.AdminUserRole))
+            {
+                model = model.Where(u => u.UserId.Equals(userid));
+            }
+
+            return View(model.ToList().ToPagedList(pageNumber ?? 1, 5));
         }
 
         //Get
@@ -32,8 +86,8 @@ namespace QuirkyBookRental.Controllers
             {
                 BookRentalViewModel model = new BookRentalViewModel
                 {
-                    Title=title,
-                    ISBN=ISBN
+                    Title = title,
+                    ISBN = ISBN
                 };
             }
             return View(new BookRentalViewModel());
@@ -43,15 +97,15 @@ namespace QuirkyBookRental.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(BookRentalViewModel bookRent)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 var email = bookRent.Email;
                 var userDetails = from u in db.Users
                                   where u.Email.Equals(email)
-                                  select new { u.Id};
+                                  select new { u.Id };
                 var ISBN = bookRent.ISBN;
 
-                Book bookSelected = db.Books.Where(b=> b.ISBN==ISBN).FirstOrDefault();
+                Book bookSelected = db.Books.Where(b => b.ISBN == ISBN).FirstOrDefault();
                 var rentalDuration = bookRent.RentalDuration;
                 var chargeRate = from u in db.Users
                                  join m in db.MembershipTypes on u.MembershipTypeId equals m.Id
@@ -63,7 +117,7 @@ namespace QuirkyBookRental.Controllers
 
                 double rentalPr = 0;
 
-                if(bookRent.RentalDuration==SD.SixMonth)
+                if (bookRent.RentalDuration == SD.SixMonth)
                 {
                     rentalPr = sixMonthRental;
                 }
@@ -74,12 +128,12 @@ namespace QuirkyBookRental.Controllers
 
                 BookRent modelToAssToDb = new BookRent
                 {
-                    BookId=bookSelected.Id,
-                    RentalPrice= rentalPr,
-                    ScheduledEndDate= bookRent.ScheduledEndDate,
-                    RentalDuration=bookRent.RentalDuration,
-                    Status=BookRent.StatusEmum.Requested,
-                    UserId=userDetails.ToList()[0].Id
+                    BookId = bookSelected.Id,
+                    RentalPrice = rentalPr,
+                    ScheduledEndDate = bookRent.ScheduledEndDate,
+                    RentalDuration = bookRent.RentalDuration,
+                    Status = BookRent.StatusEmum.Approved,
+                    UserId = userDetails.ToList()[0].Id
                 };
 
                 bookSelected.Avaibility -= 1;
@@ -90,6 +144,52 @@ namespace QuirkyBookRental.Controllers
             }
             return View();
         }
+
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Reserve(BookRentalViewModel book)
+        {
+            string userid = User.Identity.GetUserId();
+            Book bookToRent = db.Books.Find(book.BookId);
+            double rentalPr = 0;
+
+            if (userid != null)
+            {
+                var chargeRate = from u in db.Users
+                                 join m in db.MembershipTypes on u.MembershipTypeId equals m.Id
+                                 where u.Id.Equals(userid)
+                                 select new { m.ChargeRateOneMonth, m.ChargeRateSixMonth };
+
+                if (book.RentalDuration == SD.SixMonth)
+                {
+                    rentalPr = Convert.ToDouble(bookToRent.Price) * Convert.ToDouble(chargeRate.ToList()[0].ChargeRateSixMonth) / 100;
+                }
+                else
+                {
+                    rentalPr = Convert.ToDouble(bookToRent.Price) * Convert.ToDouble(chargeRate.ToList()[0].ChargeRateOneMonth) / 100;
+                }
+
+                BookRent bookRent = new BookRent
+                {
+                    BookId = bookToRent.Id,
+                    UserId = userid,
+                    RentalDuration = book.RentalDuration,
+                    RentalPrice = rentalPr,
+                    Status = BookRent.StatusEmum.Requested
+                };
+                db.BookRental.Add(bookRent);
+                var bookInDb = db.Books.SingleOrDefault(c => c.Id == book.BookId);
+                bookInDb.Avaibility -= 1;
+                db.SaveChanges();
+                return RedirectToAction("Index", "BookRent");
+            }
+
+            return View();
+        }
+
+
 
         protected override void Dispose(bool disposing)
         {
