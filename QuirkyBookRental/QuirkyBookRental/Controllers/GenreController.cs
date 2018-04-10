67366ef1a3a5a7845b0e2b00358d9f6222758437
Roberto <roberto.diperna@skylabs.it>
2018-04-10 14:45:05 +0200

    AddReference

--- QuirkyBookRental/QuirkyBookRental/Controllers/GenreController.cs
+++ QuirkyBookRental/QuirkyBookRental/Controllers/GenreController.cs
@@ -1,4 +1,5 @@
 ﻿using QuirkyBookRental.Models;
+using QuirkyBookRental.Utility;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
--- QuirkyBookRental/QuirkyBookRental/Controllers/MembershipTypeController.cs
+++ QuirkyBookRental/QuirkyBookRental/Controllers/MembershipTypeController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using QuirkyBookRental.Models;
+using QuirkyBookRental.Utility;
 
 namespace QuirkyBookRental.Controllers
 {
